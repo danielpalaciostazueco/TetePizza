0d7ea984fbd84f013bacc88cee3387d74b7abe90
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-01-18 17:11:55 +0100

    Usuario añadido a pedidos.

--- Model/Pedidos.cs
+++ Model/Pedidos.cs
@@ -5,5 +5,5 @@ public class Pedidos
     public int IdOrder { get; set; }
     public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
     public double Price{ get; set; }
-    public string? User { get; set; }
+    public Usuario? User { get; set; }
 }
---
+++ Model/Usuario.cs
@@ -0,0 +1,9 @@
+namespace TetePizza.Model;
+
+public class Usuario
+{
+    public int IdUser { get; set; }
+    public string?  NameUser { get; set; }
+
+    public string? AdressUser { get; set; }
+}
